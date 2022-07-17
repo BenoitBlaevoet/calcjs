383ad1cdf7b1ec95f53549596f7a625c291edb71
Blaevoet Benoit <blaevoet.benoit@outlook.com>
2022-07-17 16:52:54 +0200

    suppression de la fonction ready inutile

--- assets/js/calc.js
+++ assets/js/calc.js
@@ -82,25 +82,4 @@ function setOnclickEvent() {
     })
 }
 
-
-
-/***
- * Waiting the document to be ready to be manipulated by javascript
- ***/
-function ready(fn) {
-    if (document.readyState != 'loading') {
-        fn();
-    } else if (document.addEventListener) {
-        document.addEventListener('DOMContentLoaded', fn);
-    } else {
-        document.attachEvent('onreadystatechange', function() {
-        if (document.readyState != 'loading')
-            fn();
-        });
-    }
-}
-  
-  // test
-/*window.ready(function() {
-});*/
 setOnclickEvent();
